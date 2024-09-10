c0fde1b339724e88d4795a3fbf5ff5942c807080
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2024-09-10 07:23:19 -0300

    feat: add functionality to create a trip in the database

---
+++ src/Journey.Api/Controllers/TripController.cs
@@ -0,0 +1,33 @@
+﻿using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            try
+            {
+                var useCase = new RegisterTripUseCase();
+
+                var response = useCase.Execute(request);
+
+                return Created(string.Empty, response);
+            }
+            catch (JourneyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Unknown error");
+            }
+        }
+    }
+}
