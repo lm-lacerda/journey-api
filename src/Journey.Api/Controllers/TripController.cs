31a79c39263c7e5127d8a19cbd9c7f43cbe92f85
Lucas M. Lacerda <lucas.lacerda.lg@gmail.com>
2024-09-15 07:47:37 -0300

    feat: add functionality to create a trip activity in the database

--- src/Journey.Api/Controllers/TripController.cs
+++ src/Journey.Api/Controllers/TripController.cs
@@ -1,4 +1,5 @@
-﻿using Journey.Application.UseCases.Trips.Delete;
+﻿using Journey.Application.UseCases.Activities.Register;
+using Journey.Application.UseCases.Trips.Delete;
 using Journey.Application.UseCases.Trips.GetAll;
 using Journey.Application.UseCases.Trips.GetById;
 using Journey.Application.UseCases.Trips.Register;
@@ -59,5 +60,21 @@ public class TripController : ControllerBase
         useCase.Execute(id);
 
         return NoContent();
+    }
+
+    [HttpPost]
+    [Route("{tripId}/activity")]
+    [ProducesResponseType(typeof(ResponseActivityJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ResponseErrorsJson), StatusCodes.Status404NotFound)]
+    public IActionResult RegisterActivity(
+        [FromRoute] Guid tripId,
+        [FromBody] RequestRegisterActivityJson request)
+    {
+        var useCase = new RegisterActivityForTripUseCase();
+
+        var response = useCase.Execute(tripId, request);
+
+        return Created(string.Empty, response);
     }
 }
--- src/Journey.Infrastructure/JourneyDbContext.cs
+++ src/Journey.Infrastructure/JourneyDbContext.cs
@@ -5,16 +5,10 @@ namespace Journey.Infrastructure;
 public class JourneyDbContext : DbContext
 {
     public DbSet<Trip> Trips { get; set; }
+    public DbSet<Activity> Activities { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlite("Data Source=C:\\Workstation\\Journey\\JourneyDatabase.db");
-    }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Activity>().ToTable("Activities");
     }
 }
